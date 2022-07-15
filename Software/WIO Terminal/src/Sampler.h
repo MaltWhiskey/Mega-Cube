212d466dece05c68922c957cb91eaefeb42690b5
MaltWhiskey <MaltWhiskey@outlook.com>
2022-07-15 03:24:50 +0200

    include

--- Software/WIO Terminal/src/Sampler.h
+++ Software/WIO Terminal/src/Sampler.h
@@ -1,19 +1,16 @@
+#include <stdint.h>
 #pragma once
 
-class Sampler
-{
-private:
+class Sampler {
+ private:
   int sample_rate;
   int buffer_size;
   int16_t *buffer;
   int current_index;
 
-public:
+ public:
   Sampler(int sample_rate, int buffer_size);
   void start();
-  int16_t *get_buffer()
-  {
-    return buffer;
-  }
+  int16_t *get_buffer() { return buffer; }
   friend void timerIsr();
 };
